303191ec5aa537c2e0a70e19178d7b4ce46701aa
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-01-26 22:34:25 +0100

    wxcercise completed

--- index.ts
+++ index.ts
@@ -12,12 +12,44 @@ app.get("/", async (req, res) => {
   res.json(planets)
 })
 
+app.get("/planets/:id", async (req, res) => {
+  const planetID = req.params.id
+  const planet = await prisma.planets.findUnique({
+    where: {
+      id: +planetID
+    }
+  })
+
+  res.json(planet)
+})
+
 app.post("/", validate({ body: planetSchema }), async (req, res) => {
   const body: PlanetData = req.body
   const planet = await prisma.planets.create({
     data: {...body}
   })
   res.json(planet)
+})
+
+app.put("/planets/:id",async (req, res) => {
+  const planetID = req.params.id
+  const updatedData = req.body
+
+  const updatedPlanet = await prisma.planets.update({
+    where: {id: +planetID},
+    data: {...updatedData}
+  })
+
+  res.json(updatedPlanet)
+})
+
+app.delete("/planets/:id",async (req, res) => {
+  const planetID = +req.params.id
+  const deletedPlanet = await prisma.planets.delete({
+    where: {id: planetID}
+  })
+
+  res.json(deletedPlanet)
 })
 
 app.use(validationErrorMiddleware)
