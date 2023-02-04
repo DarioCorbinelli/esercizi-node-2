c4943049ede44079ecf44679f9748167bb3dab8c
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-02-04 17:16:35 +0100

    excercise completed

--- file.js
+++ file.js
@@ -1,23 +1,38 @@
-class Text {
-  constructor(text) {
-    this.string = text;
+class Bike {
+  constructor() {
+    this.wheel1 = null
+    this.wheel2 = null
+  }
+  
+  setWheel1(wheel) {
+    this.wheel1 = wheel
+  }
+  setWheel2(wheel) {
+    this.wheel2 = wheel
   }
 
-  toString() {
-    return this.string;
+  specification() {
+    let message = `${this.wheel1.label} wheel diameter = ${this.wheel1.diameter}`;
+    message += `, ${this.wheel2.label} wheel diameter = ${this.wheel2.diameter}`;
+
+    return message;
   }
 }
 
-class Shout {
-  constructor(text) {
-    this.text = text;
-  }
-
-  toString() {
-    return this.text.toString().toUpperCase()
+class Wheel {
+  constructor(label, diameter) {
+    this.label = label;
+    this.diameter = diameter;
   }
 }
 
-console.log(new Text("Hello, I'm talking").toString());
+const frontWheel = new Wheel("Front", 126);
+const backWheel = new Wheel("Back", 42);
 
-console.log(new Shout(new Text("Hello, I'm shouting!")).toString());
+const bike = new Bike();
+bike.setWheel1(frontWheel)
+bike.setWheel2(backWheel)
+
+console.log(bike);
+
+console.log("Bike specification:", bike.specification());
