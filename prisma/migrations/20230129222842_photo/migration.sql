b0c7d76b83f87df2f2740e54303e31b6c51ecd0d
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-02-04 16:01:33 +0100

    clean codebase

--- prisma/migrations/20230129222842_photo/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "planets" ADD COLUMN     "photoFilename" TEXT;
--- prisma/migrations/20230131184830_planet_user/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "planets" ADD COLUMN     "createdBy" VARCHAR(255),
-ADD COLUMN     "updatedBy" VARCHAR(255);
